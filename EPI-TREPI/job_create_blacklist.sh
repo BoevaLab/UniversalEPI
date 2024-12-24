c99ae5a698ebb50229d82c5c180b20453c569990
Aayush Grover <aayush.grover@inf.ethz.ch>
2024-12-24 06:06:23 +0100

    Please enter the commit message for your changes. Lines starting

--- EPI-TREPI/job_create_blacklist.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#SBATCH --job-name=hg38_blacklist
-#SBATCH --output=/cluster/work/boeva/tacisu/trepi/job_output/hg38-data-swap-blacklist.out.txt
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=400G
-#SBATCH --time=10:00:00
- 
-python ~/mutiger-explore/EPI-TREPI/create_blacklist.py
